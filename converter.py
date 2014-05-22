7bac969e47a59cdc2e0c95a88f314ce219c7aef4
Jason Decastro <jasonrdecastro@gmail.com>
2014-05-22 15:33:13 -0700

    PEP8 changes

--- converter.py
+++ converter.py
@@ -1,34 +1,22 @@
 from mongoTools import *
 
-dbName = "ANS"
-tableName = "ANS"
+dbName, tableName = "ANS"
+
 
 subject = "Subject"
-
 posts = MongoAdmin(dbName).getTable(tableName).posts
-
 s_ids = posts.distinct(str(subject)) 
 
 for s_id in s_ids:
-	for row in posts.find({str(subject) : s_id}):
-
-		try:
-
-			##BEGIN LOGIC
-
-			if row['Stimulus_RESP'] == row['CorrectResp']:	
-				row['ACC'] = 1
-			else:
-				row['ACC'] = 0
-
-
-			##END LOGIC
-		except KeyError:
-			print "Skipping row, no key"
-
-		except:
-			raise
-
-		print row
-
-		posts.save(row)
+    for row in posts.find({str(subject) : s_id}):
+        try:
+	    if row['Stimulus_RESP'] == row['CorrectResp']:	
+		row['ACC'] = 1
+	    else:
+		row['ACC'] = 0
+	except KeyError:
+	    print "Skipping row, no key"
+	except:
+	    raise
+	    print row
+	    posts.save(row)
